500b00bbba882c0b2d181e098c37a5580fa81f69
coreym7 <coreyrm21@gmail.com>
2025-06-14 07:55:44 -0500

    starting feature branch to allow mid war saves but prevent duplicate attacks

--- src/fetch_latest_data.py
+++ src/fetch_latest_data.py
@@ -1,11 +1,14 @@
 from utils import fetch_all_clan_data, save_json
 from dotenv import load_dotenv
 import os
+import urllib.parse
+
 
 load_dotenv()
 CLAN_TAG = os.getenv("CLAN_TAG")
+ENCODED_CLAN_TAG = urllib.parse.quote(CLAN_TAG)
 
-roster_data, war_data = fetch_all_clan_data(CLAN_TAG)
+roster_data, war_data = fetch_all_clan_data(ENCODED_CLAN_TAG)
 
 if roster_data and war_data:
     save_json(roster_data, "clan_roster.json")
